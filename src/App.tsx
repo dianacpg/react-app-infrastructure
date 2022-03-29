8105701363f7136cc8938f237e12db41bbb976cd
dianacpg <dianacpgouveia@gmail.com>
2022-03-29 18:02:10 +0100

    fix: rm svg image and change app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React, { useState } from "react";
 import { hot } from "react-hot-loader";
 import './app.scss';
-import Alert from './alert.svg'
 
 const App = () => {
 
-    const [counter, setCounter] = useState<number>(0)
-
-    const handleClick = () => {
-        setCounter(counter + 1)
-    };
-
     return (
         <div className="App">
-            <Alert width={120} height={120} />
-            <h1>I'm configuring setting up Webpack!</h1>
-            <p>{`The count now is: ${counter}`}</p>
-            <button onClick={handleClick}>Click me</button>
-            <Alert width={120} height={120} />
-            <Alert width={120} height={120} />
+            <h1>React app!</h1>
         </div>
     );
 }
