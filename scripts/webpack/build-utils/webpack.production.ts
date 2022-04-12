c28a86ffbd07dd44dc2ca5b12f57c2be36b70175
dianacpg <dianacpgouveia@gmail.com>
2022-04-12 08:05:17 +0100

    chore: add cross browser compatibility postsccs

--- scripts/webpack/build-utils/webpack.production.ts
+++ scripts/webpack/build-utils/webpack.production.ts
@@ -2,34 +2,37 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = 
-   {
+module.exports = {
   cache: true,
-  devtool: 'source-map',
-  plugins: [ 
+  devtool: "source-map",
+  plugins: [
     new HtmlWebpackPlugin({
-    template: "./src/index.html",
-    filename: "index.html",
-  }),
-  new MiniCssExtractPlugin(),
-  new CleanWebpackPlugin()],
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader,
-           // Creates `style` nodes from JS strings
-        "style-loader",
-        // Translates CSS into CommonJS
-        "css-loader"],
+        use: [
+          MiniCssExtractPlugin.loader,
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          "postcss-loader",
+        ],
       },
     ],
   },
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin({extractComments: false})],
+    minimizer: [new TerserPlugin({ extractComments: false })],
   },
 };
