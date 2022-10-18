f5a78d1272c6aef564bcbdea9b9377d819a7beff
dianacpg <dianacpgouveia@gmail.com>
2022-10-18 16:51:41 +0100

    fix: rm unnecessary hot module plugin

--- scripts/webpack/build-utils/webpack.development.ts
+++ scripts/webpack/build-utils/webpack.development.ts
@@ -1,15 +1,20 @@
-const webpack = require("webpack");
+import webpack from "webpack";
 
-module.exports = 
-   {
-    devtool: "eval-source-map",
-    devServer: {
-      // Enables HMR (hot module replacement) - preserve state
-      hot: true,
-      open: true,
+module.exports = {
+  devtool: "eval-source-map",
+  devServer: {
+    host: "0.0.0.0",
+    historyApiFallback: true,
+    // Enables HMR (hot module replacement) - preserve state
+    hot: true,
+    open: true,
+    port: 1234,
+    allowedHosts: "all",
+    client: {
+      overlay: {
+        warnings: false,
+        errors: true,
+      },
     },
-    plugins: [  
-      new webpack.HotModuleReplacementPlugin(),
-    ],
-  };
-  
+  },
+};
